0b8842bdbf2d05309cd0aed9c544208c86edd3c8
Sylvia Castro <hello@sylviacastro.co>
2019-11-08 09:59:32 -0600

    adding functionality
    Added the hours the user is over or under their ideal sleep in each log statement in calculateSleepDebt().

--- calculator.js
+++ calculator.js
@@ -2,25 +2,25 @@ const getSleepHours = day => {
    switch (day) {
      case 'monday':
        return 8
-         break;
+       break;
      case 'tuesday':
        return 7
        break;
      case 'wednesday':
        return 8
-       break;
+         break;
      case 'thursday':
        return 5
-       break;
+         break;
      case 'friday':
        return 8
-       break;
+         break;
      case 'saturday':
        return 8
-       break;
+         break;
      case 'sunday':
        return 8
-       break;
+         break;
      default:
        return "Error!"
    }
@@ -30,7 +30,7 @@ const getActualSleepHours = () =>
   getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') +
   getSleepHours('sunday');
 
-const getIdealSleepHours = () =>{
+const getIdealSleepHours = () => {
   const idealHours = 8;
   return idealHours * 7;
 }
@@ -38,16 +38,24 @@ const getIdealSleepHours = () =>{
 const calculateSleepDebt = () => {
   const actualSleepHours = getActualSleepHours();
   const idealSleepHours = getIdealSleepHours();
+ 
   if(actualSleepHours === idealSleepHours){
-      return console.log(`You received ${actualSleepHours} hours of sleep this week, which is the perfect amount of sleep!`);
-  }
-   if(actualSleepHours > idealSleepHours){
-      return console.log(`You received ${actualSleepHours} hours of sleep this week, which is more than amount of sleep you needed!`);
-  }
-   if(actualSleepHours < idealSleepHours){
-        return console.log(`You received ${actualSleepHours} hours of sleep this week, which is less than amount of sleep you needed!`);
+      return console.log(`You received ${actualSleepHours} hours of sleep this week. That is the perfect amount of sleep!`);
   } 
+  
+  else if(actualSleepHours > idealSleepHours){
+      return console.log(`Congratulations.You got ${actualSleepHours} hours of sleep this week which is ${actualSleepHours - idealSleepHours} hour(s) more than you needed!`);
+  } 
+  
+  else if(actualSleepHours < idealSleepHours){
+        return console.log(`You received ${actualSleepHours} hours of sleep this week, which is ${idealSleepHours - actualSleepHours} hours less than your required sleep. You should get some rest.`);
+  } 
+  else {
+    console.log("Error! Something went wrong. Check your code.")
+  }
 }
 
+
 calculateSleepDebt();
+
 
