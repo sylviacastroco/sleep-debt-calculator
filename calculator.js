23b113deba43646ef4426edb85dc33fa10a523c6
Sylvia Castro <hello@sylviacastro.co>
2019-11-08 08:47:31 -0600

    updated switch statement

--- calculator.js
+++ calculator.js
@@ -1,19 +1,28 @@
 const getSleepHours = day => {
    switch (day) {
      case 'monday':
-       return 8;
+       return 8
+         break;
      case 'tuesday':
-       return 8;
-       case 'wednesday':
-       return 8;
-       case 'thursday':
-       return 8;
-       case 'friday':
-       return 8;
-       case 'saturday':
-       return 8;
-       case 'sunday':
-       return 8;
+       return 7
+       break;
+     case 'wednesday':
+       return 8
+       break;
+     case 'thursday':
+       return 5
+       break;
+     case 'friday':
+       return 8
+       break;
+     case 'saturday':
+       return 8
+       break;
+     case 'sunday':
+       return 8
+       break;
+     default:
+       return "Error!""
    }
 }
 
